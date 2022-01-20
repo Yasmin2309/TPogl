3100c26bc089e80a88478fdf6a8bc120654e6ce8
Yasmin2309 <iz_mahdi@esi.dz>
2022-01-20 11:49:16 +0100

    new version

---
+++ src/main/java/com/telly/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.telly.controllers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+
+@Controller
+public class UserController {
+
+
+
+	@RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+	
+
+	
+
+}
+
+
+
